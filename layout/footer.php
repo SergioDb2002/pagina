5b6148e76d11607f08b537cbfda3e57a37953dd8
SergioDb2002 <127244010+SergioDb2002@users.noreply.github.com>
2023-03-07 16:28:01 -0300

    Add files via upload

---
+++ layout/footer.php
@@ -0,0 +1,42 @@
+<footer class="pie-pagina">
+        <div class="grupo-1">
+            <div class="box">
+                <figure>
+                    <a href="#">
+                        <img src="img/kisspng-logo-fitness-centre-brand-sport-gym-5acdc0eebc9374.5835765915234337107724.png" alt="Logo de SLee Dw">
+                    </a>
+                </figure>
+                <div class="datos">
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odit debitis, quaerat quae aperiam.</p>
+                    <div class="ubicacion">
+                        <p>Lincoln, Park
+                        Chicago, Illinions
+                        </p>
+                    </div>
+                    <div class="nro">
+                        <p>(3704) 456589
+                        (3705) 569878
+                        </p>
+                    </div>
+                    <div class="correo">
+                        <p>sadkl@gmail.com
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="box">
+                <h2>RECENT BLOG POST</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio, ipsa?</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio, ipsa?</p>
+            </div>
+            <div class="box">
+                <h2>FOLLOW</h2>
+            </div>
+        </div>
+        <div class="grupo-2">
+            <small>&copy; 2020 <b>CopyRight</b> - All .</small>
+        </div>
+    </footer>
+</body>
+</html>
---
+++ layout/header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/styles.css">
+    <title>Página Web</title>
+</head>
+<body>
+    <nav class="menu">
+        <div class="logo">
+            <img src="img/kisspng-logo-fitness-centre-brand-sport-gym-5acdc0eebc9374.5835765915234337107724.png">
+        </div>
+        <div class="contenedor">
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="about.php">About Us</a></li>
+                <li><a href="trainers.php">Trainers</a></li>
+                <li><a href="contacto.html">Shop</a></li>
+                <li><a href="contacto">Blog</a></li>
+                <li><a href="contacto">Contact Us</a></li>
+            </ul>
+        </div>
+    </nav>
