3654e1b9d650f3be9b8603ff1854ecb23b2aa18b
SergioDb2002 <127244010+SergioDb2002@users.noreply.github.com>
2023-03-07 15:52:35 -0300

    Add files via upload

---
+++ about.php
@@ -0,0 +1,39 @@
+<?php require("layout/header.php") ?>
+
+    <div class="text_about">
+        <h1>ABOUT US</h1>
+    </div>
+
+    <div class="text_desc">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque expedita, 
+        totam harum porro unde nisi omn.</p>
+
+    </div>
+
+    <div class="contenedor_box">
+        <div class="card">
+            <div class="icono_box">
+                <img src="img/grupo.png">
+            </div>
+            <h1>WINNER COACHES</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+        </div>
+
+        <div class="card">
+            <div class="icono_box">
+                <img src="img/grupo.png">
+            </div>
+            <h1>WINNER COACHES</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+        </div>
+
+        <div class="card">
+            <div class="icono_box">
+                <img src="img/grupo.png">
+            </div>
+            <h1>WINNER COACHES</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+        </div>
+    </div>
+
+    <?php require("layout/footer.php") ?>
---
+++ trainers.php
@@ -0,0 +1,101 @@
+<?php require("layout/header.php") ?>
+
+<div class="contenedor_train">
+    <div class="columnas">
+        <img src="img/1.jpg">
+        <div class="textImg1">
+            <h1>BODY BUILDING</h1>
+            <P>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloribus placeat veritatis possimus.</P>
+            <button class="boton1">READ MORE</button>
+        </div>
+        <img src="img/2.jpg">
+        <div class="textImg2">
+            <h1>CARDIO</h1>
+            <P>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloribus placeat veritatis possimus.</P>
+            <button class="boton2">READ MORE</button>
+        </div>
+    </div>
+
+    <div class="columnas">
+        <img src="img/3.jpg">
+        <div class="textImg3">
+            <h1>FITNESS</h1>
+            <P>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloribus placeat veritatis possimus.</P>
+            <button class="boton3">READ MORE</button>
+        </div>
+        <img src="img/4.jpg">
+        <div class="textImg4">
+            <h1>CROSSFIT</h1>
+            <P>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloribus placeat veritatis possimus.</P>
+            <button class="boton4">READ MORE</button>
+        </div>
+    </div>
+</div>
+
+
+<div class="titulo_train">
+    <h1>OUR ACHIEVEMENTS</h1>
+</div>
+
+<div class="estadisticas">
+    <div class="est">
+        <h1>19</h1>
+    </div>
+    <img src="img/circulo.png">
+
+    <div class="est">
+        <h1>150</h1>
+    </div>
+    <img src="img/circulo.png">
+
+    <div class="est">
+        <h1>879</h1>
+    </div>
+    <img src="img/circulo.png">
+
+    <div class="est">
+        <h1>7</h1>
+    </div>
+    <img src="img/circulo.png">
+</div>
+
+<div class="sub1">
+    <h1>TRAINNING COURSES</h1>
+</div>
+<div class="sub2">
+    <h1>TRAINNING COURSES</h1>
+</div>
+<div class="sub3">
+    <h1>TRAINNING COURSES</h1>
+</div>
+<div class="sub4">
+    <h1>TRAINNING COURSES</h1>
+</div>
+
+<div class="titulo_train">
+    <h1>MEET OUR TRAINERS</h1>
+</div>
+
+<div class="contenedor_train">
+    <div class="train_img">
+        <img src="img/train.jpg">
+        <p>ROSY RIVERA</p>
+    </div>
+
+    <div class="train_img">
+        <img src="img/train.jpg">
+        <p>ROSY RIVERA</p>
+    </div>
+
+    <div class="train_img">
+        <img src="img/train.jpg">
+        <p>ROSY RIVERA</p>
+    </div>
+
+    <div class="train_img">
+        <img src="img/train.jpg">
+        <p>ROSY RIVERA</p>
+    </div>
+</div>
+
+<?php require("layout/footer.php") ?>
